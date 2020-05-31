7c2cf2519ba8a58335eff07fbf3b97b983c29c2e
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 20:42:43 -0500

    Writting the menu

---
+++ Else_IF.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	string input;
+
+	cout<<"1. A"<<endl;
+	cout<<"2. B"<<endl;
+	cout<<"3. C"<<endl;
+	cout<<"4. D"<<endl;
+	cout<<"5. E"<<endl;
+
+	cout<<"\n"<<"Press one number"<<endl;
+
+	return 0;
+}
